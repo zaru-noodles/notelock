a6f1e62a42d17a5e7b5b46fa33b935ee7179b908
Aidan Tay <silver14211@gmail.com>
2026-05-21 20:49:12 +0800

    moved page styling to layout.tsx

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -3,5 +3,9 @@ export default function PublicLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <>{children}</>;
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-linear-to-br from-purple-200 to-blue-400/50 px-4">
+      {children}
+    </div>
+  );
 }
--- app/(public)/register-page/page.tsx
+++ app/(public)/register-page/page.tsx
@@ -1,9 +1,5 @@
 import RegisterForm from "@/app/components/auth/RegisterForm";
 
 export default function Register() {
-  return (
-    <div className="flex flex-col justify-center items-center h-screen bg-linear-to-br from-purple-200 to-blue-400/50 px-4">
-      <RegisterForm />
-    </div>
-  );
+  return <RegisterForm />;
 }
