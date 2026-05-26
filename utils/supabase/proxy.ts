47b877c52f4111107fe144e283b6c60166bbf953
qixiang oh <ohqixiang@gmail.com>
2026-05-26 22:03:33 +0800

    Change proxy to landing page

--- utils/supabase/proxy.ts
+++ utils/supabase/proxy.ts
@@ -54,7 +54,7 @@ export async function updateSession(request: NextRequest) {
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
-    url.pathname = "/login-page";
+    url.pathname = "/";
     return NextResponse.redirect(url);
   }
 
