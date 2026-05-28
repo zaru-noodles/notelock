02650df7a5027c88636a34f9ccadaaec1ef9f008
qixiang oh <ohqixiang@gmail.com>
2026-05-28 18:57:06 +0800

    added value to both email input

--- app/components/auth/LoginForm.tsx
+++ app/components/auth/LoginForm.tsx
@@ -47,6 +47,7 @@ export default function LoginForm() {
       <input
         className="border border-gray-300 rounded px-3 py-2 focus:border-blue-500 focus:outline-none"
         type="email"
+        value={loginRequest.email}
         onChange={(e) =>
           setLoginRequest({
             ...loginRequest,
--- app/components/auth/RegisterForm.tsx
+++ app/components/auth/RegisterForm.tsx
@@ -55,6 +55,7 @@ export default function RegisterForm({ setLoginInfomation }: Props) {
       <input
         className="border border-gray-300 rounded px-3 py-2 focus:border-blue-500 focus:outline-none"
         type="email"
+        value={registerRequest.email}
         onChange={(e) =>
           setRegisterRequest({
             ...registerRequest,
