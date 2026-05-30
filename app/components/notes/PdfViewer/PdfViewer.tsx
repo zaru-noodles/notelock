ba669a5778c7caf90bafd88eb108ef8676cf007a
qxxd <ohqixiang@gmail.com>
2026-05-31 03:35:35 +0800

    Self hosted pdfjs from mozilla, added localhost to HOSED_VIEWER_ORIGIN, still need to add vercel url to HOSTED_VIEWER_ORIGIN

--- app/components/notes/PdfViewer/PdfViewer.tsx
+++ app/components/notes/PdfViewer/PdfViewer.tsx
@@ -4,7 +4,7 @@ export default function PdfViewer({ url }: { url: string }) {
   return (
     <div className="border border-paper-3 bg-paper-0 rounded-md shadow-sh-2">
       <iframe
-        src={`https://mozilla.github.io/pdf.js/web/viewer.html?file=${encodeURIComponent(url)}`}
+        src={`/pdfjs/web/viewer.html?file=${encodeURIComponent(url)}`}
         className="w-full h-screen border border-paper-3 rounded-md"
         title="PDF viewer"
       />
