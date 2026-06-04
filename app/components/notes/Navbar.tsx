634c99019fa2981c6425aa99e5138f5149aa4626
Aidan <silver14211@gmail.com>
2026-06-04 18:04:38 +0800

    added user info to navbar and shifted logout button from dashboard to nav

--- app/components/notes/Navbar.tsx
+++ app/components/notes/Navbar.tsx
@@ -2,9 +2,24 @@
 
 import { useRouter } from "next/navigation";
 import ModuleInputNavbar from "./ModuleInputNavbar";
+import { createClient } from "@/utils/supabase/client";
+import { useEffect, useState } from "react";
+import LogoutButton from "../auth/LogoutButton";
 
 export default function Navbar() {
   const router = useRouter();
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    async function getUser() {
+      const db = createClient();
+      const {
+        data: { user },
+      } = await db.auth.getUser();
+      setUsername(user?.user_metadata?.username ?? "");
+    }
+    getUser();
+  }, []);
 
   function selectModule(moduleCode: string) {
     if (moduleCode !== "") {
@@ -16,6 +31,11 @@ export default function Navbar() {
     <nav className="sticky top-0 z-10 flex items-center gap-5.5 px-8 pt-4 pb-3 border-b border-paper-3 bg-paper-2/80 backdrop-blur-md">
       <h1 className="text-4xl leading-none translate-y-1">NoteLock</h1>
       <ModuleInputNavbar onChange={selectModule} />
+      <div className="border-r border-terra-400 pr-1.5">
+        <p className="text-right leading-none pb-0.5">{username}</p>
+        <p className="text-sm text-gray-600 text-right leading-none">Student</p>
+      </div>
+      <LogoutButton />
     </nav>
   );
 }
