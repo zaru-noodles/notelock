e0b329589942e61739a07e7c417f6379098a7260
qixiang oh <ohqixiang@gmail.com>
2026-06-04 23:25:58 +0800

    Fixed minor styling detail where header moved when toggling between signup and login

--- app/components/landing-page/Hero.tsx
+++ app/components/landing-page/Hero.tsx
@@ -3,7 +3,7 @@ import AuthCard from "./AuthCard";
 export default function Hero() {
   return (
     <section id="login" className="paper-bg w-full">
-      <div className="relative xl:px-12 px-6 pt-18 pb-22 w-full max-w-7xl xl:w-[70%] mx-auto grid grid-cols-1 xl:grid-cols-[minmax(0,1.05fr)_minmax(0,420px)] gap-14 items-center xl:content-center justify-items-center xl:justify-items-start">
+      <div className="relative xl:px-12 px-6 pt-18 pb-22 w-full max-w-7xl xl:w-[70%] mx-auto grid grid-cols-1 xl:grid-cols-[minmax(0,1.05fr)_minmax(0,420px)] gap-14 items-start xl:content-center justify-items-center xl:justify-items-start">
         {/* Left Side with design */}
         <div className="max-w-145">
           <h1 className="font-medium xl:text-[86px] text-5xl text-ink-0 mb-7 leading-none tracking-tight">
