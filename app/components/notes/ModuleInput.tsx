db468b56756cd7cf919df8083d1805d107e1c151
Aidan <silver14211@gmail.com>
2026-06-04 17:25:20 +0800

    updated styling for module and semester input

--- app/components/notes/ModuleInput.tsx
+++ app/components/notes/ModuleInput.tsx
@@ -1,15 +1,22 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import type { Module } from "@/types/index";
 
 type Props = {
   onChange: (input: string) => void;
+  autocomplete?: boolean;
+  placeholder?: string;
 };
 
-export default function ModuleInput({ onChange }: Props) {
+export default function ModuleInput({
+  onChange,
+  autocomplete = true,
+  placeholder = "Module",
+}: Props) {
   const [textInput, setTextInput] = useState("");
   const [modules, setModules] = useState<Module[]>([]);
+  const clickedItem = useRef(false);
 
   useEffect(() => {
     if (textInput.length == 0) {
@@ -17,7 +24,7 @@ export default function ModuleInput({ onChange }: Props) {
     }
 
     async function searchModules() {
-      const response = await fetch(`/api/modules?search=${textInput}&count=5`);
+      const response = await fetch(`/api/modules?search=${textInput}&count=8`);
       const data = await response.json();
 
       if (response.ok) {
@@ -37,25 +44,38 @@ export default function ModuleInput({ onChange }: Props) {
 
   function resetModule() {
     setTextInput("");
+    setModules([]);
     onChange("");
   }
 
   return (
-    <div>
+    <div className="w-120">
       <input
+        className="w-120 px-4 py-2 text-2x1 rounded-2xl bg-paper-3 text-sm placeholder-gray-600 border border-transparent focus:outline-none focus:border-terra-200 focus:bg-paper-2 transition-all duration-200"
         type="text"
         value={textInput}
-        onBlur={() =>
-          modules.length != 0 ? setModule(modules[0]) : resetModule()
-        }
+        onBlur={() => {
+          // ignore blur if li was clicked
+          if (clickedItem.current) {
+            clickedItem.current = false;
+            return;
+          }
+          modules.length != 0 && autocomplete
+            ? setModule(modules[0])
+            : resetModule();
+        }}
         onChange={(e) => setTextInput(e.target.value)}
-        placeholder="Module"
+        placeholder={placeholder}
       />
 
-      <ul>
+      <ul className="absolute bg-paper-2 shadow-lg z-50 mt-1">
         {modules.map((module) => (
           <li
+            className="px-4 py-2 text-sm text-gray-800 hover:bg-paper-1 cursor-pointer transition-colors duration-100"
             key={module.id}
+            onMouseDown={() => {
+              clickedItem.current = true;
+            }}
             onClick={() => {
               setModule(module);
               setModules([]);
--- app/components/notes/Navbar.tsx
+++ app/components/notes/Navbar.tsx
@@ -1,7 +1,15 @@
+"use client";
+import ModuleInput from "./ModuleInput";
+
 export default function Navbar() {
   return (
     <nav className="sticky top-0 z-10 flex items-center gap-5.5 px-8 pt-4 pb-1 border-b border-paper-3 bg-paper-1/80 backdrop-blur-md">
       <h1 className="text-4xl">NoteLock</h1>
+      <ModuleInput
+        onChange={() => {}}
+        autocomplete={false}
+        placeholder="Search modules..."
+      />
     </nav>
   );
 }
