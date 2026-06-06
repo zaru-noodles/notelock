8a728583a762773177ce11259e746ca97a0e5c58
Aidan <silver14211@gmail.com>
2026-06-06 19:23:10 +0800

    made note panel clickable, pushing to note page

--- app/components/notes/NotePanel.tsx
+++ app/components/notes/NotePanel.tsx
@@ -1,12 +1,19 @@
 import { Note } from "@/types";
+import { useRouter, usePathname } from "next/navigation";
 
 type Props = {
   noteData: Note;
 };
 
 export default function NotePanel({ noteData }: Props) {
+  const router = useRouter();
+  const pathname = usePathname();
+
   return (
-    <div className="w-80 h-fit mx-6 my-2 p-4 bg-terra-50 rounded-1x1 border border-terra-200 rounded-2xl">
+    <div
+      className="w-80 h-fit mx-6 my-2 p-4 bg-paper-2 hover:bg-paper-3 rounded-1x1 border border-terra-100 rounded-2xl transition-transform duration-200 hover:-translate-y-px hover:shadow-sh-4"
+      onClick={() => router.push(`${pathname}/${noteData.id}`)}
+    >
       <img className="w-auto h-40" alt="Note image here" />
       <div className="flex justify-between items-start mb-0">
         <h2 className="font-semibold text-lg">{noteData.title}</h2>
