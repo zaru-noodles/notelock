8001e5a47a14043e05ab5e88362420546ab12751
Aidan <silver14211@gmail.com>
2026-06-08 14:14:23 +0800

    styling

--- app/components/notes/NotePanel.tsx
+++ app/components/notes/NotePanel.tsx
@@ -12,14 +12,20 @@ export default function NotePanel({ noteData }: Props) {
 
   return (
     <div
-      className="w-80 h-fit mx-6 my-2 p-4 bg-paper-2 hover:bg-paper-3 rounded-1x1 border border-terra-100 rounded-2xl transition-transform duration-200 hover:-translate-y-px hover:shadow-sh-4"
+      className="w-80 h-fit mx-4.5 my-3 p-4 bg-paper-2 hover:bg-paper-3 rounded-1x1 border border-terra-100 rounded-2xl transition-transform duration-200 hover:-translate-y-px hover:shadow-sh-4"
       onClick={() => router.push(`${pathname}/${noteData.id}`)}
     >
-      <img
-        className="w-auto h-40"
-        alt="Note image here"
-        src={noteData.thumbnailUrl}
-      />
+      <div className="flex justify-center items-cente rounded-xl mb-3 h-44 overflow-hidden">
+        {noteData.thumbnailUrl && (
+          <img
+            className="h-40 w-auto object-contain rounded-lg shadow-sm"
+            alt="Missing thumbnail"
+            src={noteData.thumbnailUrl}
+          />
+        )}
+
+        {!noteData.thumbnailUrl && <p>Missing thumbnail</p>}
+      </div>
       <div className="flex justify-between items-start mb-0">
         <h2 className="font-semibold text-lg">{noteData.title}</h2>
 
--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["pdf-to-img"],
-  },
+  serverExternalPackages: ["pdf-to-img"],
 };
 
 export default nextConfig;
