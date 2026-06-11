82e19c05fc9effaac7c0c3584e8e1445387a27e0
Aidan <silver14211@gmail.com>
2026-06-11 17:28:13 +0800

    styled upload page

--- app/(protected)/upload/page.tsx
+++ app/(protected)/upload/page.tsx
@@ -2,9 +2,11 @@ import UploadForm from "@/app/components/notes/UploadForm";
 
 export default function Upload() {
   return (
-    <>
-      <h2>Upload notes</h2>
-      <UploadForm />
-    </>
+    <div className="justify-center m-13">
+      <h2 className="text-5xl mb-0.5">Upload Notes</h2>
+      <div className="flex h-[75vh] items-center justify-center">
+        <UploadForm />
+      </div>
+    </div>
   );
 }
