be7be14f96bedeef94d7a2cdf555b9839a7c201d
qixiang oh <ohqixiang@gmail.com>
2026-06-15 23:29:41 +0800

    Installed and configured msw

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,10 @@
 import "@testing-library/jest-dom";
+import { cleanup } from "@testing-library/react";
+import { server } from "@/mocks/node";
+
+beforeAll(() => server.listen());
+afterEach(() => {
+  server.resetHandlers();
+  cleanup();
+});
+afterAll(() => server.close());
