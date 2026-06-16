d7a4c73e854fcec5add4998de818c47a6d2f1ea7
qixiang oh <ohqixiang@gmail.com>
2026-06-17 05:01:15 +0800

    Added e2e test for registering and reaching dashboard

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -5,6 +5,20 @@ test("landing page loads", async ({ page }) => {
   await expect(page.getByRole("button", { name: "Login" })).toBeVisible();
 });
 
-test("creating an account works", async ({ page }) => {
+test("new user can register and reach dashboard", async ({ page }) => {
+  const email = `${Date.now()}@u.nus.edu`;
+  const username = `${Date.now()}u`;
+  const password = "password123!";
+
   await page.goto("/");
+
+  await page.getByRole("button", { name: "Sign up" }).click();
+  await page.getByPlaceholder("e0123456@u.nus.edu").fill(email);
+  await page.getByPlaceholder("••••••••").nth(0).fill(password);
+  await page.getByPlaceholder("••••••••").nth(1).fill(password);
+  await page.getByPlaceholder("Zaru").fill(username);
+
+  await page.getByRole("button", { name: "Create an account" }).click();
+
+  await page.waitForURL("**/dashboard");
 });
