ce38da270c963bd206ba3a707ddb526464667876
Aidan <silver14211@gmail.com>
2026-07-23 14:42:47 +0800

    added auth listener

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from "../components/notes/navbar/Navbar";
 import { Toaster } from "react-hot-toast";
 import { redirect } from "next/navigation";
 import { getCurrentUser } from "../api/auth/current-user";
+import { AuthListener } from "../components/auth/AuthListener";
 export default async function ProtectedLayout({
   children,
 }: Readonly<{
@@ -11,6 +12,7 @@ export default async function ProtectedLayout({
   if (!user) redirect("/");
   return (
     <>
+      <AuthListener />
       <Navbar
         userID={user.id}
         username={user.username}
